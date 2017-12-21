a5b4fec9397e184cda6e58d4b1f59663d960d908
alchermd <johnalcherdoloiras@gmail.com>
2017-12-22 01:31:30 +0800

    Renamed the previous index route to whoami  , since an index page is now rendered.

--- header_parser/views.py
+++ header_parser/views.py
@@ -1,12 +1,17 @@
 import re
 
-from flask import jsonify, request
+from flask import jsonify, render_template, request
 
 from . import app
 
 
-@app.route('/api/whoami')
+@app.route('/')
 def index():
+    return render_template('index.html')
+
+
+@app.route('/api/whoami')
+def whoami():
     # The User-Agent string is quite lengthy, and we only need the 'software'
     # part. So we'll use the re library to parse the string with regular expressions.
     software_regex = re.compile(r"\(.+?\)")
